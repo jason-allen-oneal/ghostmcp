c6b6985bdbf54b9cfebec475a94d7e957cf9e906
Jason O'Neal <jason.allen.oneal@gmail.com>
2026-07-26 19:45:23 -0400

    docs: mark repository as moved

--- ghostmcp/parsers/__init__.py
+++
@@ -1,6 +0,0 @@
-"""GhostMCP Output Parsers Package."""
-
-# Parser functions are imported explicitly to avoid namespace pollution
-# Use: from ghostmcp.parsers.nmap import parse_nmap_xml
-#      from ghostmcp.parsers.nuclei import parse_nuclei_json
-# etc.
